10827eb3d7394e608a6cc0dfed20ae0f92540561
danielrotheia <danielrotheia@gmail.com>
2022-03-31 09:19:03 -0300

    Update modelRNA.py

--- modelRNA.py
+++ modelRNA.py
@@ -32,32 +32,32 @@ def create_model():
     return model
 
 
-def validation(df_test: pd.DataFrame, df_predito: pd.DataFrame):
+def validation(df_test: pd.DataFrame, df_pred: pd.DataFrame):
     x_real = df_test['x2']
-    x_previsto = df_predito['x2']
-    z_previsto = df_predito['z2']
+    x_pred = df_pred['x2']
+    z_pred = df_pred['z2']
     
-    rmse_x = mean_squared_error(x_real, x_previsto) ** (1 / 2)
-    _, _, r_value, _, _ = stats.linregress(x_real, x_previsto)
+    rmse_x = mean_squared_error(x_real, x_pred) ** (1 / 2)
+    _, _, r_value, _, _ = stats.linregress(x_real, x_pred)
     r2_x = r_value*r_value
-    mae_x = mean_absolute_error(x_real, x_previsto)
-    mse_x = mean_squared_error(x_real, x_previsto)
+    mae_x = mean_absolute_error(x_real, x_pred)
+    mse_x = mean_squared_error(x_real, x_pred)
 
     y_real = df_test['y2']
-    y_previsto = df_predito['y2']
-    rmse_y = mean_squared_error(y_real, y_previsto) ** (1 / 2)
-    _, _, r_value, _, _ = stats.linregress(y_real, y_previsto)
+    y_pred = df_pred['y2']
+    rmse_y = mean_squared_error(y_real, y_pred) ** (1 / 2)
+    _, _, r_value, _, _ = stats.linregress(y_real, y_pred)
     r2_y = r_value*r_value
-    mae_y = mean_absolute_error(y_real, y_previsto)
-    mse_y = mean_squared_error(y_real, y_previsto)
+    mae_y = mean_absolute_error(y_real, y_pred)
+    mse_y = mean_squared_error(y_real, y_pred)
     
     z_real = df_test['z2']
-    z_previsto = df_predito['z2']
-    rmse_z = mean_squared_error(z_real, z_previsto) ** (1 / 2)
-    _, _, r_value, _, _ = stats.linregress(z_real, z_previsto)
+    z_pred = df_pred['z2']
+    rmse_z = mean_squared_error(z_real, z_pred) ** (1 / 2)
+    _, _, r_value, _, _ = stats.linregress(z_real, z_pred)
     r2_z = r_value*r_value
-    mae_z = mean_absolute_error(z_real, z_previsto)
-    mse_z = mean_squared_error(z_real, z_previsto)
+    mae_z = mean_absolute_error(z_real, z_pred)
+    mse_z = mean_squared_error(z_real, z_pred)
 
     return (r2_x, r2_y, r2_z), (rmse_x, rmse_y, rmse_z), (mae_x, mae_y, mae_z), (mse_x, mse_y, mse_z)
 
@@ -81,27 +81,26 @@ def normalize(row):
     return {'x': norm_x, 'y': norm_y, 'z': norm_z}
 
 
-def desnormalize(row):
+def denormalize(row):
     if row['x'] >= 0:
-        desnorm_x = row['x'] * max_x
+        denorm_x = row['x'] * max_x
     else:
-        desnorm_x = row['x'] * -min_x
+        denorm_x = row['x'] * -min_x
 
     if row['y'] >= 0:
-        desnorm_y = row['y'] * max_y
+        denorm_y = row['y'] * max_y
     else:
-        desnorm_y = row['y'] * -min_y
+        denorm_y = row['y'] * -min_y
         
     if row['z'] >= 0:
-        desnorm_z = row['z'] * max_z
+        denorm_z = row['z'] * max_z
     else:
-        desnorm_z = row['z'] * -min_z
+        denorm_z = row['z'] * -min_z
 
-    return {'x': desnorm_x, 'y': desnorm_y, 'z': desnorm_z}
+    return {'x': denorm_x, 'y': denorm_y, 'z': denorm_z}
 
 
-
-def splitData(df: pd.DataFrame, p_train: float, window_size = 3):
+def splitData(df: pd.DataFrame, p_train: float, window_size=3):
     x_start_train = []
     y_start_train = []
     z_start_train = []
@@ -214,34 +213,35 @@ file = "real.csv"
 env = file.split(".")[0]
 
 if env == "real":
-    dados = pd.read_csv(f"./datasets/{file}", sep=',', usecols=['x', 'y'])
-    dados['z'] = 1
+    data = pd.read_csv(f"./datasets/{file}", sep=',', usecols=['x', 'y'])
+    data['z'] = 1
     size = 15500
     
 else:
-    dados = pd.read_csv(f"./datasets/{file}", sep=';', usecols=['x', 'y', 'z'])
-    #Can be any size until 185000
+    data = pd.read_csv(f"./datasets/{file}", sep=';', usecols=['x', 'y', 'z'])
+    #Can be any size until 170000
     size = 50000
 
-min_x = dados.x.min()
-max_x = dados.x.max()
-min_y = dados.y.min()
-max_y = dados.y.max()
-min_z = dados.z.min()
-max_z = dados.z.max()
+min_x = data.x.min()
+max_x = data.x.max()
+min_y = data.y.min()
+max_y = data.y.max()
+min_z = data.z.min()
+max_z = data.z.max()
 
 size_str = str(int(size/1000)) + "k"
 
-dados_norm = dados.apply(lambda row: normalize(row), axis=1)
-dados_norm = pd.DataFrame(dados_norm.tolist())
+data_norm = data.apply(lambda row: normalize(row), axis=1)
+data_norm = pd.DataFrame(data_norm.tolist())
 
+data_train = data_norm[:size]
 if env == "real":
-    dados_train = dados_norm[:size]
+    data_test = data_norm[size:]
 else:
-    dados_teste = dados_norm[-100000: ]
+    data_test = data_norm[-100000: ]
     
-dados_teste = dados_norm[size: ]
-dados_teste.reset_index(inplace=True)
+data_test = data_norm[size: ]
+data_test.reset_index(inplace=True)
 
 percentage_train = 0.8
 times = 10
@@ -254,7 +254,7 @@ mse_model = []
 rmse_model = []
 mae_model = []
 
-path = f"./validation/{env}/rna"
+path = f"./validation/{env}/ann"
 for w in range(3, 33):   
     r2_exec = {'x': [], 'y': [], 'z': []}
     mse_exec = {'x': [], 'y': [], 'z': []}
@@ -263,41 +263,44 @@ for w in range(3, 33):
     
     max_t = w - 1
     
-    print("Splitting the data...")
-    df_feat_train, df_target_train, df_feat_val, df_target_val = splitData(dados_train, percentage_train, window_size=w)
-    df_feat_test, df_target_test, _, _ = splitData(dados_teste, 1, window_size=w)
+    print("splitting the data...")
+    df_feat_train, df_target_train, df_feat_val, df_target_val = splitData(data_train, percentage_train, window_size=w)
+    df_feat_test, df_target_test, _, _ = splitData(data_test, 1, window_size=w)
         
     df_target_test.columns = ['x', 'y', 'z']
-    df_target_test = df_target_test.apply(lambda row: desnormalize(row), axis=1)
+    df_target_test = df_target_test.apply(lambda row: denormalize(row), axis=1)
     df_target_test = pd.DataFrame(df_target_test.tolist())
     df_target_test.columns = ['x2', 'y2', 'z2']
     
     df_feat_train.info()
     
     for i in range(times):
-        print(w, f"- {i + 1}ª vez")
+        print(w, f"- {i + 1} time")
            
-        print("modelling...")
+        print("modeling...")
         model = create_model()
         history = model.fit(df_feat_train, df_target_train, validation_data=(df_feat_val, df_target_val), epochs=epochs, callbacks=callbacks)
-        model.save(f"./validacao/modelos/{env}/rna/model_d{w-2}")
+
+        if i == 0:
+            print("saving model...")
+            model.save(f"./validation/models/{env}/ann/model_d{w-2}")
         
         predicted_points = model.predict(df_feat_test)
         
-        df_predito = pd.DataFrame(predicted_points, columns = ['x', 'y', 'z'])
+        df_pred = pd.DataFrame(predicted_points, columns = ['x', 'y', 'z'])
         
-        df_predito = df_predito.apply(lambda row: desnormalize(row), axis=1)
-        df_predito = pd.DataFrame(df_predito.tolist())
-        df_predito.columns = ['x2', 'y2', 'z2']
+        df_pred = df_pred.apply(lambda row: denormalize(row), axis=1)
+        df_pred = pd.DataFrame(df_pred.tolist())
+        df_pred.columns = ['x2', 'y2', 'z2']
 
-        model_indicators = validation(df_target_test, df_predito)
+        model_indicators = validation(df_target_test, df_pred)
         
+        print("validating...")
         (r2_x, r2_y, r2_z), (rmse_x, rmse_y, rmse_z), (mae_x, mae_y, mae_z), (mse_x, mse_y, mse_z) = model_indicators
         print(f"R2: x={r2_x}, y={r2_y}, z={r2_z}")
         print(f"RMSE: x={rmse_x}, y={rmse_y}, z={rmse_z}")
         print(f"MAE: x={mae_x}, y={mae_y}, z={mae_z}")
         print(f"MSE: x={mse_x}, y={mse_y}, z={mse_z}")
-        exit()
         
         r2_exec['x'].append(r2_x)
         r2_exec['y'].append(r2_y)
@@ -315,13 +318,14 @@ for w in range(3, 33):
         mae_exec['y'].append(mae_y)
         mae_exec['z'].append(mae_z)
         
+        print("saving...")
         pd.DataFrame(r2_exec).to_csv(f"{path}/r2_{size_str}_d{w-2}.csv")
         pd.DataFrame(mse_exec).to_csv(f"{path}/mse_{size_str}_d{w-2}.csv")
         pd.DataFrame(rmse_exec).to_csv(f"{path}/rmse_{size_str}_d{w-2}.csv")
         pd.DataFrame(mae_exec).to_csv(f"{path}/mae_{size_str}_d{w-2}.csv")
 
         del model
-        del df_predito
+        del df_pred
         
     del df_feat_train
     del df_target_train
